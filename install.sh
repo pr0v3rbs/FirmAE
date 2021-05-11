53701af3615d0caa015e41ed2d5804edee7ff3c7
pr0v3rbs <rla5072@paran.com>
2021-05-12 00:18:20 +0900

    Fix binwalk installation
    Issue #20

--- install.sh
+++ install.sh
@@ -22,11 +22,16 @@ python3 -m pip install psycopg2 psycopg2-binary
 sudo apt-get install -y busybox-static bash-static fakeroot dmsetup kpartx netcat-openbsd nmap python3-psycopg2 snmp uml-utilities util-linux vlan
 
 # for binwalk
-sudo apt-get install -y binwalk mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract fusecram cramfsswap squashfs-tools sleuthkit default-jdk cpio lzop lzma srecord zlib1g-dev liblzma-dev liblzo2-dev unzip
+curl -Ls https://api.github.com/repos/ReFirmLabs/binwalk/releases/latest | \
+  grep -wo "\"https.*tarball.*\"" | sed 's/"//g' | wget -qi - -O binwalk.tar.gz && \
+  tar -xf binwalk.tar.gz && \
+  cd ReFirmLabs-binwalk-*/ && \
+  sudo python3 setup.py install
+sudo apt-get install -y mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract fusecram cramfsswap squashfs-tools sleuthkit default-jdk cpio lzop lzma srecord zlib1g-dev liblzma-dev liblzo2-dev unzip
 
 git clone https://github.com/devttys0/sasquatch && (cd sasquatch && ./build.sh && cd -)
 git clone https://github.com/devttys0/yaffshiv && (cd yaffshiv && sudo python3 setup.py install)
-cp core/unstuff /usr/local/bin/
+sudo cp core/unstuff /usr/local/bin/
 
 python3 -m pip install python-lzo cstruct git+https://github.com/sviehb/jefferson ubi_reader
 sudo apt-get install -y python3-magic openjdk-8-jdk unrar
