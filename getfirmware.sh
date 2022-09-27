e890b16e451587c1fd660d11613318f26f5eb2cc
Mick <105726899+n0s3y@users.noreply.github.com>
2022-09-27 02:42:02 -0700

    Update getfirmware.sh

--- getfirmware.sh
+++ getfirmware.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 wget http://files.dlink.com.au/products/DIR-895L/REV_A/Firmware/Firmware_v1.13b03/DIR895LA1_FW113b03.bin
 echo  "De testfirmware genaamd 'DIR-8905L' zou nu in jouw wd moeten staan."
+sudo chmod +x getfirmware.sh
 exit
