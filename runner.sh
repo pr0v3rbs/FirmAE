608e5ac4f00a7de16e7b381e561b7665e576c4f8
Mick <105726899+n0s3y@users.noreply.github.com>
2022-11-01 15:42:56 +0100

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -5,11 +5,11 @@ if [ -z  "$FILE" ]
 then
     exit 1
 fi
-
+BRAND=$(zenity --entry --title="Add new profile" --text="Enter name of new profile:")
 if zenity --question --title="FirmAE firmware runner" --text="Would you like to open this file with the FirmAE Debugger?"
 then
     ./init.sh
-    sudo ./run.sh -d ${FILE}
+    sudo ./run.sh -d ${BRAND} ${FILE}
 else
     zenity --text-info --filename="$FILE" --title="Firmware runner"
 fi
