04c78aea5b95a76b321087ff3dc759bc1285f9f0
Mick <105726899+n0s3y@users.noreply.github.com>
2022-11-01 15:40:29 +0100

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -6,14 +6,10 @@ then
     exit 1
 fi
 
-BRAND=$(zenity --entry --entry-text="Type the brand of the firmware" --text="Tell me...")
-
-ANALYZE=$(zenity --list  --title "Choose Processor" --radiolist  --column "ID" --column="Name" 1 -c 2 -d 3 -a)
-
 if zenity --question --title="FirmAE firmware runner" --text="Would you like to open this file with the FirmAE Debugger?"
 then
     ./init.sh
-    sudo ./run.sh ${ANALYZE} ${FILE}
+    sudo ./run.sh -d ${FILE}
 else
     zenity --text-info --filename="$FILE" --title="Firmware runner"
 fi
